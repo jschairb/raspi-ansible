96e29b3cf36c1ad47012bed6df07d9736a1003d3
Joshua Schairbaum <joshua.schairbaum@gmail.com>
2014-07-19 13:22:22 -0500

    correct git error, change install dir

--- install.sh
+++ install.sh
@@ -5,8 +5,9 @@ pip install ansible
 
 dir=/etc/raspi-ansible
 hosts=${dir}/hosts
+repo_dir=${dir}/repo
 
 mkdir -p ${dir}
 echo "[pi]\n$(uname -n)" > ${hosts}
 
-ansible-pull -U https://github.com/jschairb/raspi-ansible.git -i ${hosts} -d ${dir} site.yml
+ansible-pull -U https://github.com/jschairb/raspi-ansible.git -i ${hosts} -d ${repo_dir} site.yml
